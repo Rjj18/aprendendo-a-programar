db6cd7aa46532a311fb6c011c53d87958b1c1a55
Roger Oliveira <roger.psn12@gmail.com>
2022-08-26 12:16:03 -0200

    pratica 1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,10 @@
+let nome = "Roger";
+let email = "roger.psn12@gmail.com"
+let profissão = "Professor"
+let idade = 35
+let cidadeSonho = "Tóquio"
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${cidadeSonho}. 
+Confirma pra gente alguns dados? A sua idade é ${idade}, 
+você é ${profissão} e o seu email é ${email}? 
+Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
